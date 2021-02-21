c11410ba206429fdf1dfa6c1e8833a907fa0bee4
Gil <heg2@njit.edu>
2021-02-21 17:21:23 -0500

    Feature: Setup calculator with results test

---
+++ src/Calculator.py
@@ -0,0 +1,19 @@
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        x = 2 + 2
+        self.result = x
+        pass
+
+    def add(self, a, b):
+        return self.result(a, b)
+
+    def subtract(self, a, b):
+        return self.result(a, b)
+
+    def multiply(self, a, b):
+        return self.result(a, b)
+
+    def divide(self, a, b):
+        return self.result(a, b)
