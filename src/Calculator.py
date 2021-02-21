46ed774270b041d16c3360238274763397e08b93
Gil <heg2@njit.edu>
2021-02-21 17:44:13 -0500

    Feature: Setup calculator methods

--- src/Calculator.py
+++ src/Calculator.py
@@ -2,18 +2,23 @@ class Calculator:
     result = 0
 
     def __init__(self):
-        x = 2 + 2
-        self.result = x
         pass
 
     def add(self, a, b):
-        return self.result(a, b)
+        self.result = a + b
+        return a + b
 
     def subtract(self, a, b):
-        return self.result(a, b)
+        return a - b
 
     def multiply(self, a, b):
-        return self.result(a, b)
+        return a * b
 
     def divide(self, a, b):
-        return self.result(a, b)
+        return a / b
+
+    def square(self, a):
+        return a ** 2
+
+    def sqrt(self, a, b):
+        return a ** .5
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -3,14 +3,16 @@ from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
+    calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
 
     def test_results_property_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(self.calculator.result, 4)
+
+    def test_add_method_calculator(self):
+        self.assertEqual(self.calculator.add(2, 2), 4)
 
 
 if __name__ == '__main__':
