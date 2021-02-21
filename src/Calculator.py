c40a9ed608cd1ddb9e7342940febc9b60d4850b6
Gil <heg2@njit.edu>
2021-02-21 18:32:28 -0500

    Feature: Setup 7 successful tests

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,11 +1,9 @@
 class Calculator:
-    result = 0
 
     def __init__(self):
         pass
 
     def add(self, a, b):
-        self.result = a + b
         return a + b
 
     def subtract(self, a, b):
@@ -20,5 +18,5 @@ class Calculator:
     def square(self, a):
         return a ** 2
 
-    def sqrt(self, a, b):
+    def sqrt(self, a):
         return a ** .5
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -3,16 +3,30 @@ from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
-    calculator = Calculator()
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
-    def test_results_property_calculator(self):
-        self.assertEqual(self.calculator.result, 4)
-
     def test_add_method_calculator(self):
         self.assertEqual(self.calculator.add(2, 2), 4)
+
+    def test_subtract_method_calculator(self):
+        self.assertEqual(self.calculator.subtract(4, 2), 2)
+
+    def test_multiply_method_calculator(self):
+        self.assertEqual(self.calculator.multiply(2, 2), 4)
+
+    def test_divide_method_calculator(self):
+        self.assertEqual(self.calculator.divide(8, 2), 4)
+
+    def test_square_method_calculator(self):
+        self.assertEqual(self.calculator.square(3), 9)
+
+    def test_sqrt_method_calculator(self):
+        self.assertEqual(self.calculator.sqrt(25), 5)
 
 
 if __name__ == '__main__':
