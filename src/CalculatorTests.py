c1073f738b43dd7559fefefff29157d11db959d2
Gil <heg2@njit.edu>
2021-02-22 08:54:10 -0500

    Task: moved csv files to independent folder

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -12,32 +12,32 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.calculator, Calculator)
 
     def test_add_method_calculator(self):
-        reader = CSVReader('/src/Addition.csv').data
+        reader = CSVReader('/src/csv/Addition.csv').data
         for row in reader:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
 
     def test_subtract_method_calculator(self):
-        reader = CSVReader('/src/Subtraction.csv').data
+        reader = CSVReader('/src/csv/Subtraction.csv').data
         for row in reader:
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
 
     def test_multiply_method_calculator(self):
-        reader = CSVReader('/src/Multiplication.csv').data
+        reader = CSVReader('/src/csv/Multiplication.csv').data
         for row in reader:
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
 
     def test_divide_method_calculator(self):
-        reader = CSVReader('/src/Division.csv').data
+        reader = CSVReader('/src/csv/Division.csv').data
         for row in reader:
             self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), round(float(row['Result']), 9))
 
     def test_square_method_calculator(self):
-        reader = CSVReader('/src/Square.csv').data
+        reader = CSVReader('/src/csv/Square.csv').data
         for row in reader:
             self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
 
     def test_sqrt_method_calculator(self):
-        reader = CSVReader('/src/SquareRoot.csv').data
+        reader = CSVReader('/src/csv/SquareRoot.csv').data
         for row in reader:
             self.assertEqual(self.calculator.sqrt(row['Value 1']), round(float(row['Result']), 9))
 
