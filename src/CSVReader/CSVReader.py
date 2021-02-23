78dcaba51e35cf9656738e79f7a0ecabbba63d95
Gil <heg2@njit.edu>
2021-02-23 09:14:33 -0500

    Task: moved CSVReader.py to independent folder

--- src/CSVReader/CSVReader.py
+++ src/CSVReader/CSVReader.py
@@ -5,9 +5,10 @@ class CSVReader:
     data = []
 
     def __init__(self, filepath):
+        self.data = []
+
         with open(filepath) as text_data:
             reader = csv.DictReader(text_data)
             for row in reader:
                 self.data.append(row)
-        self.data.clear()
         pass
